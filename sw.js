a8281edb0d36b7b10d7f11cf483c891dcc213d0d
harshavarthanep <113576943+harshavarthanep@users.noreply.github.com>
2026-03-19 14:25:37 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'zenwealth-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+  // Add icon-192.png and icon-512.png here once you have them
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
